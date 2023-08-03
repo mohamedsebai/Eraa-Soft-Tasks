063295a8eba27b1fdd02c4917de89ae9914d4ce9
mohamed sebai <mohamedseabeai@gmail.com>
2023-08-03 06:51:24 +0300

    added new pms databse with many feature

--- EraaSoftTaskEight/pms_with_orders.sql
+++ EraaSoftTaskEight/pms_with_orders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2023 at 03:31 PM
+-- Generation Time: Aug 03, 2023 at 05:48 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `carts` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -39,9 +39,11 @@ CREATE TABLE `carts` (
 -- Dumping data for table `carts`
 --
 
-INSERT INTO `carts` (`id`, `user_id`, `order_id`, `created_at`, `update_at`) VALUES
-(1, 1, 1, '2023-07-28 20:45:05', '2023-07-28 22:44:55'),
-(2, 2, 2, '2023-07-28 20:45:27', '2023-07-28 22:45:22');
+INSERT INTO `carts` (`id`, `user_id`, `product_id`, `created_at`, `update_at`) VALUES
+(11, 13, 11, '2023-08-03 01:14:27', '0000-00-00 00:00:00'),
+(14, 13, 12, '2023-08-03 01:35:10', '0000-00-00 00:00:00'),
+(15, 13, 12, '2023-08-03 01:37:32', '0000-00-00 00:00:00'),
+(16, 13, 12, '2023-08-03 01:42:31', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -63,8 +65,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `cat_name`, `cat_description`, `user_id`, `created_at`, `update_at`) VALUES
-(1, 'Sport', 'for all who have interest in sport, bla bla bla', 1, '2023-07-25 13:23:57', '2023-07-25 15:22:25'),
-(2, 'Elctronics', 'for all who have interest in elctronics, bla bla bla', 2, '2023-07-25 13:23:57', '2023-07-25 15:22:25');
+(14, 'elecortincs', 'for all who have interest in elctronics, bla bla bla, bbbbb', 6, '2023-08-01 00:06:43', '0000-00-00 00:00:00'),
+(15, 'sports', 'for all who have interest in sports, bla bla bla, bbbbb', 6, '2023-08-01 15:02:34', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -76,6 +78,7 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
   `users_id` int(11) DEFAULT NULL,
+  `size` varchar(50) NOT NULL,
   `price` varchar(50) DEFAULT NULL,
   `quantity` varchar(50) DEFAULT NULL,
   `discount` varchar(50) DEFAULT NULL,
@@ -88,9 +91,11 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `product_id`, `users_id`, `price`, `quantity`, `discount`, `total`, `created_at`, `update_at`) VALUES
-(1, 1, 1, '1000$', '2', '0%', '2000$', '2023-07-28 20:43:35', '2023-07-28 22:42:24'),
-(2, 2, 2, '5000$', '5', '0%', '25000$', '2023-07-28 20:44:39', '2023-07-28 22:44:02');
+INSERT INTO `orders` (`id`, `product_id`, `users_id`, `size`, `price`, `quantity`, `discount`, `total`, `created_at`, `update_at`) VALUES
+(14, 11, 13, 's', '100$', '2', '20%', '180$', '2023-08-03 03:41:18', '0000-00-00 00:00:00'),
+(15, 11, 13, 's', '100$', '2', '20%', '160$', '2023-08-03 03:45:39', '0000-00-00 00:00:00'),
+(17, 11, 1, 's', '100$', '3', '20%', '240$', '2023-08-03 03:46:06', '0000-00-00 00:00:00'),
+(18, 11, 6, 's', '100$', '4', '20%', '320$', '2023-08-03 03:46:34', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -124,6 +129,7 @@ INSERT INTO `phones` (`id`, `user_id`, `country_code`, `phone_number`, `created_
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `prd_name` varchar(255) NOT NULL,
+  `prd_quantity` varchar(50) NOT NULL,
   `prd_price` varchar(50) NOT NULL,
   `prd_brand_name` varchar(255) DEFAULT NULL,
   `prd_discount` varchar(50) DEFAULT NULL,
@@ -132,16 +138,19 @@ CREATE TABLE `products` (
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_date` datetime DEFAULT NULL
+  `update_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `prd_name`, `prd_price`, `prd_brand_name`, `prd_discount`, `prd_size`, `prd_image`, `user_id`, `category_id`, `created_at`, `update_date`) VALUES
-(1, 'RMD-teshrit-new-kit-2023', '1000$', 'Nike', '0%', 'm', 'real-md-kit.jpg', 1, 1, '2023-07-25 13:25:31', NULL),
-(2, 'pc-new-with-nivida-gpu', '5000$', 'HP', '20%', NULL, 'pc-gpu-nivida.jpg', 2, 2, '2023-07-25 13:26:54', NULL);
+INSERT INTO `products` (`id`, `prd_name`, `prd_quantity`, `prd_price`, `prd_brand_name`, `prd_discount`, `prd_size`, `prd_image`, `user_id`, `category_id`, `created_at`, `update_at`) VALUES
+(6, 'FCB_TSHIRT', '9', '100$', 'NIKE', '0%', 'xxl', 'IMG-1979497394.jpg', 6, 15, '2023-08-01 15:59:15', NULL),
+(9, 'FCB_TSHIRT', '10', '100$', 'NIKE', '0%', '', 'IMG-1979497394.jpg', 6, 14, '2023-08-01 16:01:50', NULL),
+(10, 'FCB_TSHIRT', '20', '100$', 'NIKE', '0%', 'xxl', 'IMG-890166524.jpg', 6, 14, '2023-08-01 16:38:48', NULL),
+(11, 'RMD_TSHIRT', '100', '100$', 'PUMA', '20%', 'xxl', 'IMG-2119150098.jpeg', 13, 15, '2023-08-02 23:32:05', NULL),
+(12, 'FCB_TSHIRT', '400', '100$', 'NIKE', '20%', NULL, 'IMG-329009484.jpg', 13, 15, '2023-08-03 01:30:57', NULL);
 
 -- --------------------------------------------------------
 
@@ -166,7 +175,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `profile_img`, `role`, `created_at`, `update_at`) VALUES
 (1, 'mohamedsebai', 'mohamedseabeai@gmail.com', '123456', 'img-ms.jpg', 'admin', '2023-07-25 13:21:12', '2023-07-25 15:20:42'),
-(2, 'mohamed khalifa', 'mohamedkhalifa@gmail.com', '12345', 'img-kh.jpg', 'admin', '2023-07-25 13:22:08', '2023-07-25 15:21:23');
+(2, 'mohamed khalifa', 'mohamedkhalifa@gmail.com', '12345', 'img-kh.jpg', 'admin', '2023-07-25 13:22:08', '2023-07-25 15:21:23'),
+(6, 'mohamedseabeai', 'mohamedseabeaiss@gmail.com', '$2y$10$HKi1R3zVarJqBS6SKVqGI.UfwbO0XITdZAGek4PV6CI0eKOgfLUOm', 'IMG-549264080.jpg', 'admin', '2023-07-31 16:54:14', '0000-00-00 00:00:00'),
+(13, 'mohamedseabeaiuser', 'mohamedseabeaiuser@gmail.com', '$2y$10$qDT8ivJLuBO7ruJEYojsDeoiUWLOQydSmVcNZ6Q2yFyvLwPn2vquG', 'IMG-788209037.jpeg', 'user', '2023-08-02 23:18:50', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -178,7 +189,7 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `profile_img`, `role
 ALTER TABLE `carts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `users_conts_for_cart` (`user_id`),
-  ADD KEY `belong_to_order_id` (`order_id`);
+  ADD KEY `products_conts_for_cart` (`product_id`);
 
 --
 -- Indexes for table `categories`
@@ -224,19 +235,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `phones`
@@ -248,13 +259,13 @@ ALTER TABLE `phones`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -264,7 +275,7 @@ ALTER TABLE `users`
 -- Constraints for table `carts`
 --
 ALTER TABLE `carts`
-  ADD CONSTRAINT `belong_to_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `products_conts_for_cart` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `users_conts_for_cart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
